c7aa018d700d70b8556cde75ecf05e0f68c26e3a
madiev <andrey_1_87@mail.ru>
2020-05-27 18:31:17 +0300

    added reset in reducers

--- app/reducers/common.js
+++ app/reducers/common.js
@@ -8,19 +8,26 @@ const apiReducer = (type, prepare, extendKey, state, action) => {
         var data = {
             pending: true,
             data: state.data
-        }
+        };
         return typeof extendKey === 'function' ? Object.assign({}, state, { [extendKey(action)]: data }) : data;
     case type.SUCCESS:
         var data = {
             pending: false,
             data: prepare(action)
-        }
+        };
         return typeof extendKey === 'function' ? Object.assign({}, state, { [extendKey(action)]: data }) : data;
     case type.FAILURE:
-        return {
+        var data = {
             pending: null,
             data: undefined
         };
+        return typeof extendKey === 'function' ? {} : data;
+    case type.RESET:
+        var data = {
+            pending: null,
+            data: {}
+        };
+        return typeof extendKey === 'function' ? {} : data;
     default:
         return state;
     }
